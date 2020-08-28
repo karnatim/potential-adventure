47b03011d515d9e37dbd251e677402f79a3a07eb
David Bittner <david.r.bittner@gmail.com>
2020-08-28 19:33:35 -0400

    davids test commit

--- app/src/main/java/com/example/scheduler/MainActivity.java
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ///MAHIR ADDED SOME COMMENT
+        //MAHIR ADDED SOME COMMENT
+        //david comment
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
