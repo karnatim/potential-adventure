cb16b52327acec001676f957cf87819f5adaf72e
David Bittner <david.r.bittner@gmail.com>
2020-08-31 19:46:06 -0400

    google sign in

--- app/src/main/java/com/example/scheduler/MainActivity.java
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -1,17 +1,47 @@
 //TEST can be deleted
 package com.example.scheduler;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1;
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build();
+
+    GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+
+        });
+
 
     }
+
 }
