f8cacafe3e30e360602d4c301c96a1fc0ba3488a
Alexander Harris <awhofficial@gmail.com>
2020-09-18 16:58:19 -0400

    set up skeleton of navigation drawer. Need to switch around the layouts to make it functional though

--- app/src/main/java/com/example/scheduler/MainMenuActivity.java
+++ app/src/main/java/com/example/scheduler/MainMenuActivity.java
@@ -1,14 +1,39 @@
 package com.example.scheduler;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Notification;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu2);
+        setContentView(R.layout.activity_main_menu);
+
+
+        //Whatever hooks are
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+
+        //using toolbar as action bar
+        setSupportActionBar(toolbar);
+
+        //Change
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
